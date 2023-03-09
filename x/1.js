cff7ca4035cd9062daefc783905e17dc77798b89
6p10ku <35252223+6p10ku@users.noreply.github.com>
2023-03-09 05:03:07 +0300

    Update 1.js

--- x/1.js
+++ x/1.js
@@ -1,20 +1,14 @@
-// v 3.0 08.03.2023
-
-//проверка на utm в web_link -- если есть, то используем их как основу
+// проверка на utm в web_link -- если есть, то используем их как основу
 if (web_link_desk.search('utm_source=')>0){
     var url_params = (web_link_desk.split('?').length > 1 ? web_link_desk.split('?')[1].split('&') : '') || []
 } else {var url_params = (document.location.href.split('?').length > 1 ? document.location.href.split('?')[1].split('&') : '') || []};
 
 // global vars and funcs
-
 var date = new Date; date.setDate(date.getDate() + 30);
-//var cookie_path = ';domain=.' + location.hostname.split('.').reverse()[1] + '.' + location.hostname.split('.').reverse()[0] + '; path=/; expires=' + date.toUTCString();
-var cookie_path = ';domain=.' + '6p10ku.github.io' +'; path=/; expires=' + date.toUTCString();
+var cookie_path = ';domain=.' + location.hostname.split('.').reverse()[1] + '.' + location.hostname.split('.').reverse()[0] + '; path=/; expires=' + date.toUTCString();
 var dnow = Date.now().toString();
 var custom_uid = dnow + Math.floor(Math.random() * (600000000 - 100000000 + 1) + 100000000).toString();
-
 var dataLayerSL = [];
-
 var utm_params_arr = url_params.filter(v => v.toLowerCase().search('utm_') !== -1) || [];
 
 // func
@@ -23,23 +17,18 @@ function createCookie(name, value) {document.cookie = name + '=' + value + cooki
 function check_platform() { if (/android/gi.test(navigator.userAgent)) { return 'android'; } else if (/iphone|ipod/gi.test(navigator.userAgent)) { return 'iPhone'; } else {return 'unknown'; }};
 var platform = check_platform();
 
-
-
 // проверяем и ставим куки систем
-
-var cid = [getCookie('_ym_uid'), getCookie('_sv'), getCookie('___dmpkit___'), getCookie('top100_id')]// getCookie('adriver_uid')
+var cid = [getCookie('_ym_uid'), getCookie('_sv'), getCookie('___dmpkit___'), getCookie('top100_id')]; // getCookie('adriver_uid')
 
 if (cid[0] === '') {createCookie('_ym_uid',custom_uid); cid[0]=custom_uid};
 if (cid[1] === '') {createCookie('_sv',custom_uid); cid[1]=custom_uid};
 if (cid[2] === '') {createCookie('___dmpkit___',custom_uid); cid[2]=custom_uid};
 if (cid[3] === '') {createCookie('top100_id',custom_uid); cid[3]=custom_uid};
-//if (cid[0] === '') {createCookie('adriver_uid',custom_uid); cid[0]=custom_uid};
-
+//if (cid[0] === '') {createCookie('adriver_uid',custom_uid); cid[0]=custom_uid}; // требуется добавление
 
 // проверяем и ставим utm в куки
 if ((typeof(document.referrer) != 'undefined') && (document.referrer.search('google')>=0 && document.referrer.search('google')<=15)) {var gtmOrgEngn = 'google'};
 if ((typeof(document.referrer) != 'undefined') && (document.referrer.search('yandex')>=0 && document.referrer.search('yandex')<=15)) {var gtmOrgEngn = 'yandex'};
-
 if (document.location.href.search('utm_source') < 0 && (gtmOrgEngn)) {
     createCookie('utm_source',gtmOrgEngn); createCookie('utm_medium','organic'); createCookie('utm_campaign',''); createCookie('utm_content','');
 } else {
@@ -47,15 +36,13 @@ if (document.location.href.search('utm_source') < 0 && (gtmOrgEngn)) {
     (v.search('utm_source') != -1 ? createCookie('utm_source',v.split('=')[1]) : '') || (v.search('utm_medium') != -1 ? createCookie('utm_medium',v.split('=')[1]) : '') || (v.search('utm_campaign') != -1 ? createCookie('utm_campaign',v.split('=')[1]) : '') || (v.search('utm_content') != -1 ? createCookie('utm_content',v.split('=')[1]) : '') || (v.search('utm_term') != -1 ? createCookie('utm_term', v.split('=')[1]) : ''))
 };
 
-
 // проверка старых переменных, создание новых
 // необходимо для поддержки старого шаблона
 if (typeof(web_link) != 'undefined') {var web_link_and = web_link, web_link_ios = web_link, web_link_desk = web_link};
 
-//, store_link_and = web_link, store_link_ios = web_link;}
+//, store_link_and = web_link, store_link_ios = web_link;} // требуется обновление
 
 if (typeof(af_media_source) != 'undefined') {var sl_name = af_media_source};
-
 if (typeof(android_app) != 'undefined') {var android_app = ['ru.sberbankmobile']};
 if (typeof(ios_apps_dp) == 'undefined') {var ios_apps_dp = ['sberbankonline://','sbolonline://']};
 if (typeof(custom_params) == 'undefined') {var custom_params = false};
@@ -65,11 +52,9 @@ if (typeof(ext) == 'undefined') {var ext = true};
 if (typeof(web_link_desk) != 'undefined' && web_link_desk == '') {web_link_desk = 'http://www.sberbank.ru/ru/person/dist_services/inner_apps'};
 if (typeof(web_link_and) != 'undefined' && web_link_and == '') {web_link_and = web_link_desk};
 if (typeof(web_link_ios) != 'undefined' && web_link_ios == '') {web_link_ios = web_link_desk};
-
 if (typeof(store_link_and) != 'undefined' && store_link_and == '') {store_link_and = 'https://sberbank.com/sms/obnovy'};
 if (typeof(store_link_ios) != 'undefined' && store_link_ios == '') {store_link_ios = 'https://online.sberbank.ru/CSAFront/index.do'};
 if (typeof(ios_dp) != 'undefined' && ios_dp.search('^sberbankonline:|^sbolonline:') == 0 && ios_apps_dp[0] == '') {var ios_apps_dp = ['sberbankonline://'+ios_dp.split('://')[1], 'sbolonline://'+ios_dp.split('://')[1]]};
-
 
 // проверка параметра маршрутизации в URL
 if (document.location.href.search('need_web=true')>0) {var need_web = true};
@@ -81,10 +66,13 @@ if (document.location.href.search('external_source=false')>0) {var ext = false};
 var host_href = new URL(location).hostname.match('[[a-z0-9-_]+.[a-z]+$')[0];
 var utm_name = ['utm_source=', 'utm_medium=', 'utm_campaign=', 'utm_content=', 'utm_term='];
 
+
+
 setTimeout(() => {
 
 
-    // Куки в переменную через таймер
+
+// Куки в переменную через таймер
 var utm_cookie_arr = [getCookie('utm_source'), getCookie('utm_medium'), getCookie('utm_campaign'), getCookie('utm_content'), getCookie('utm_term')];
 
 // Функция формирования финальной WEB ссылки 
@@ -140,16 +128,15 @@ function get_final_app(dp) {
         app_data.length > 1 ? app_final = dp + '?' + app_data : app_final = dp; 
     }
     return app_final
-
 }
 
+// DataLayer 
 dataLayerSL.push = function (a, l) {
 
     var rdm = new XMLHttpRequest();
-    rdm.open('POST', document.location.protocol+'//dmp.sbermarketing.ru/?dmpkit_cid=9064fc6c-76fe-4a6d-aea6-92ef3f343257&dmpkit_tgt=2js&dmpkit_ctid=f940199e-3e2e-4fac-a636-01f1f5bf88e9&dmpkit_evn=sber_event&dmpkit_p=tm', true);
+        rdm.open('POST', document.location.protocol+'//dmp.sbermarketing.ru/?dmpkit_cid=9064fc6c-76fe-4a6d-aea6-92ef3f343257&dmpkit_tgt=2js&dmpkit_ctid=f940199e-3e2e-4fac-a636-01f1f5bf88e9&dmpkit_evn=sber_event&dmpkit_p=tm', true);
     
     var json = JSON.stringify({
-
         'uids': [[10008, 10031, cid[2]], [10008, 10037, cid[3]], [10008, 10041, cid[1]], [10008, 10060, cid[0]]], //[10008, 10055, cid[5]],
         'eventAttrs': [[301, 'SMARTLINK_'+sl_name], [302, a], [303, l],
         [10006, navigator.userAgent],
@@ -173,11 +160,11 @@ dataLayerSL.push = function (a, l) {
     })) 
 
     var rym1 = new XMLHttpRequest(); 
-    rym1.open('GET', document.location.protocol+'//mc.yandex.ru/watch/89867636?wmode=7&page-url='+encodeURIComponent(document.location.href)+'&charset=utf-8'+'&browser-info='+encodeURIComponent('pv:1:en:utf-8:u:'+cid[0]+':ns:'+dnow+':t:' + document.title), false); 
-    rym1.send(); 
+        rym1.open('GET', document.location.protocol+'//mc.yandex.ru/watch/89867636?wmode=7&page-url='+encodeURIComponent(document.location.href)+'&charset=utf-8'+'&browser-info='+encodeURIComponent('pv:1:en:utf-8:u:'+cid[0]+':ns:'+dnow+':t:' + document.title), false); 
+        rym1.send(); 
     var rym2 = new XMLHttpRequest(); 
-    rym2.open('POST', document.location.protocol+'//mc.yandex.ru/watch/89867636/1?page-url='+encodeURIComponent(document.location.href)+'&charset=utf-8'+'&browser-info='+encodeURIComponent('pv:1:en:utf-8:u:'+cid[0]+':ns:'+dnow+':t:' + document.title+'&site-info='+ym_sessions), false); 
-    rym2.send();
+        rym2.open('POST', document.location.protocol+'//mc.yandex.ru/watch/89867636/1?page-url='+encodeURIComponent(document.location.href)+'&charset=utf-8'+'&browser-info='+encodeURIComponent('pv:1:en:utf-8:u:'+cid[0]+':ns:'+dnow+':t:' + document.title+'&site-info='+ym_sessions), false); 
+        rym2.send();
 }
 
 
@@ -262,38 +249,30 @@ if (platform == 'android') {
                     setTimeout(function () { dataLayerSL.push('android','go_to_1_web_other_inner'); redirect(web_link_desk) }, 900);
                 }
             }
-        
-
-        
-    // if (browser == 'Chrome' || browser == 'YaBrowser' || browser == 'Opera' || browser == 'YaStart') {
-
-    //         var app_data = get_final_app(android_dp);
-    //         var dp = app_data.split('://');
-    //         if (need_web == false) { var web_data = get_final_web(store_link_and) } else { var web_data = get_final_web(web_link_and)};
-    //         setTimeout(function () {window.location.href = 'intent://' + dp[1] + '#Intent;scheme=' + dp[0] + ';package=' + android_app[0] + ';end'}, 100);
-
-    //     } else {
-    //         var app_data = get_final_app(android_dp);
-    //         var dp = app_data.split('://');
-    //         if (need_web == false) { var web_data = get_final_web(web_link_and) } else { var web_data = get_final_web(web_link_and)};
-    //         setTimeout(function () {window.location.href = 'intent://' + dp[1]}, 100);
-    //         setTimeout(function () {window.location.href = web_data}, 2000);;
-    //     }
 
     } else if (platform == 'iPhone') {
-        dataLayerSL.push('ios', ios_apps_dp[0]);
-        setTimeout(function() {
-            setTimeout(function() {
-                setTimeout(function() {
-                    dataLayerSL.push('ios_go_to_web', web_link_ios);
-                    window.location.href = get_final_web(web_link_ios);
-                }, 800);
-                        var web_data = get_final_web(web_link_ios);
-                        var param_for_pad = get_final_app(ios_apps_dp[0]).split(ios_apps_dp[0])[1];
-                        window.location.href = document.location.origin + '/x/redirect_pad.html?URL_START=' +  (document.location.href) + '&SL_iOS_DP_OLD=' + encodeURIComponent(ios_apps_dp[1] + param_for_pad) + '&SL_WEB_LINK_iOS=' + encodeURIComponent(web_data)
-                  }, 600);
-                        window.location.href = ios_apps_dp[0] + get_final_app(ios_apps_dp[0]).split(ios_apps_dp[0])[1];
-                     }, 300);
+
+    dataLayerSL.push('ios', ios_apps_dp[0]);
+
+    setTimeout(function () {
+        window.location.href = ios_apps_dp[0] + get_final_app(ios_apps_dp[0]).split(ios_apps_dp[0])[1];
+    },
+        300);
+
+    setTimeout(function () {
+        var web_data = get_final_web(web_link_ios);
+        var param_for_pad = get_final_app(ios_apps_dp[0]).split(ios_apps_dp[0])[1];
+        window.location.href = document.location.origin + '/x/redirect_pad.html?URL_START=' + (document.location.href) + '&SL_iOS_DP_OLD=' + encodeURIComponent(ios_apps_dp[1] + param_for_pad) + '&SL_WEB_LINK_iOS=' + encodeURIComponent(web_data)
+    },
+        600);
+
+    setTimeout(function () {
+        dataLayerSL.push('ios_go_to_web', web_link_ios);
+        window.location.href = get_final_web(web_link_ios);
+    },
+        800);
+
+                     
     } else {
         // Desktop
         dataLayerSL.push('web', web_link_desk);
@@ -303,4 +282,3 @@ if (platform == 'android') {
     }
 
     }, 300)
-    
